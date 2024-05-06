49a5a2aec774921f051a0f7400a890eed5130368
Rodrigo Rocha <rodrigorgs@gmail.com>
2024-05-05 23:08:36 -0300

    Migrate to ewelink-api-next

--- src/ewelink/ewelink.service.ts
+++ src/ewelink/ewelink.service.ts
@@ -1,27 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-const ewelink = require('ewelink-api');
+import eWeLink from "ewelink-api-next";
 
 @Injectable()
 export class EwelinkService {
-  private connection: any;
+  private client: any;
+  private user = null;
 
   constructor(private configService: ConfigService) {
-    this.connection = new ewelink({
-      email: this.configService.get<string>('EWELINK_EMAIL'),
-      password: this.configService.get<string>('EWELINK_PASSWORD'),
+    this.client = new eWeLink.WebAPI({
+      appId: 'Uw83EKZFxdif7XFXEsrpduz5YyjP7nTl',
+      appSecret: 'mXLOjea0woSMvK9gw7Fjsy7YlFO4iSu6',
       region: this.configService.get<string>('EWELINK_REGION'),
-      APP_ID: 'Uw83EKZFxdif7XFXEsrpduz5YyjP7nTl',
-      APP_SECRET: 'mXLOjea0woSMvK9gw7Fjsy7YlFO4iSu6'
     });
   }
 
+  async ensureLoggedIn() {
+    if (this.user === null) {
+      try {
+        const response = await this.client.user.login({
+          account: this.configService.get<string>('EWELINK_EMAIL'),
+          password: this.configService.get<string>('EWELINK_PASSWORD'),
+          areaCode: '+' + this.configService.get<string>('EWELINK_AREA_CODE')
+        });
+        if (response.error === 0) {
+          this.user = response.data.user;
+        }
+      } catch (err) {
+        console.log("Failed to login:", err.message);
+      }
+    }
+  }
+
   async getDevices() {
-    return this.connection.getDevices();
+    await this.ensureLoggedIn();
+    return this.client.device.getAllThings();
   }
 
   async setDevicePower(deviceId: string, state: string) {
-    return this.connection.setDevicePowerState(deviceId, state);
+    await this.ensureLoggedIn();
+    return this.client.device.setThingStatus({
+      type: 1,
+      id: deviceId,
+      params: {
+        switches: [
+          {
+            switch: state,
+            outlet: 0
+          }
+        ]
+      }
+    });
+    // this.client.setDevicePowerState(deviceId, state);
+    // return this.connection.setDevicePowerState(deviceId, state);
   }
 
   async triggerSwitch() {

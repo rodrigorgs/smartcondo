a5eda15db2c2344777ec4596f248caeb806dc4a0
Rodrigo Rocha <rodrigorgs@gmail.com>
2024-05-09 06:03:10 -0300

    Add secret key to operate switch

--- src/switch/switch.controller.ts
+++ src/switch/switch.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { EwelinkService } from 'src/ewelink/ewelink.service';
 
-@Controller('switch')
+@Controller('@mar-azul')
 export class SwitchController {
-  constructor(private ewelink : EwelinkService) {}
+  constructor(private ewelink: EwelinkService,
+      private configService: ConfigService) { }
 
   @Get('devices')
   async getDevices() {
     return this.ewelink.getDevices();
   }
 
-  @Get('power')
-  async setDevicePower() {
-    return this.ewelink.triggerSwitch();
+  @Get('portao')
+  async setDevicePower(@Query('key') key: string) {
+    console.log("Key:", key);
+    if (key === this.configService.get<string>('SECRET_CODE')) {
+      return this.ewelink.triggerSwitch();
+    } else {
+      return "Invalid key";
+    }
   }
 }

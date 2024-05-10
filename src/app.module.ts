c0193c432b26a2c72447efa54f494e681be03100
Rodrigo Rocha <rodrigorgs@gmail.com>
2024-05-10 14:21:41 -0300

    Add persistence for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { ConfigModule } from '@nestjs/config';
 import { EwelinkService } from './ewelink/ewelink.service';
 import { TodoitemsModule } from './todoitems/todoitems.module';
 import { SwitchController } from './switch/switch.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
@@ -19,6 +22,17 @@ const databaseUrl =
       rootPath: join(__dirname, '..', 'public'),
     }),
     TodoitemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'smartcondo',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
   ],
   controllers: [SwitchController],
   providers: [EwelinkService],

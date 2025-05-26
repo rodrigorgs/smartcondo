9114f2e46a4abde29b29f566ff355414e531f385
Rodrigo Rocha <rodrigorgs@gmail.com>
2025-05-25 22:24:46 -0300

    Fix creation and removal of access keys

--- src/access-keys/access-keys.service.ts
+++ src/access-keys/access-keys.service.ts
@@ -32,7 +32,13 @@ export class AccessKeysService {
     }
 
     const keyString = Math.random().toString(36).substring(2, 15);
-    const accessKey = this.accessKeysRepository.create({ ...createAccessKeyDto, keyString, condo: { id: condo.id } });
+    const objToCreate = {
+      ...createAccessKeyDto,
+      keyString,
+      user: { id: createAccessKeyDto.userId },
+      condo: { id: condo.id },
+    };
+    const accessKey = this.accessKeysRepository.create(objToCreate);
     return this.accessKeysRepository.save(accessKey);
   }
 
@@ -53,7 +59,10 @@ export class AccessKeysService {
   }
 
   findByCondoSlugAndKey(condoSlug: string, keyString: string) {
-    return this.accessKeysRepository.findOne({ where: { condo: { slug: condoSlug }, keyString } });
+    return this.accessKeysRepository.findOne({
+      where: { condo: { slug: condoSlug }, keyString },
+      relations: ['user'],
+    });
   }
 
   async findByKey(keyString: string) {
